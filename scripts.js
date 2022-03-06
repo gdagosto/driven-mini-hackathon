0f2026c989131a25cc446251bd3dc6da5766732e
Gabriel d Agosto <gabriel.dagosto@engenharia.ufjf.br>
2022-03-06 18:43:30 -0300

    all changes

--- scripts.js
+++ scripts.js
@@ -1,65 +1,88 @@
-const url = "https://api.openweathermap.org/";
+const url = "https://api.openweathermap.org";
 const apiKey = `393eeb90be3bedb143a4dd6632d378b4`;
-let lat;
-let lon;
-let cityName;
-let weatherBox = document.querySelector(".weather-box");
-let data;
-let minTemp;
-let maxTemp;
-let temp;
-let icon;
+const bodyRef = document.querySelector("body");
+const weatherBoxRef = document.querySelector(".weather-box");
+const cityInputRef = document.querySelector("#cityInput");
 
-
+function resetStyling() {
+  weatherBoxRef.style.opacity = 0;
+  bodyRef.className = "default";
+}
 
 function getMyLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition) 
-}        
+  resetStyling();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+function getCityLocation() {
+  resetStyling();
+  getApiDataFromCityName(cityInputRef.value);
+}
+
+function cityInputOnKeyDown(e) {
+  if (e.keyCode === 13) {
+    getCityLocation();
+  }
+}
+
 function showPosition(position) {
-    lat = position.coords.latitude; 
-    lon = position.coords.longitude;  
-    getApiDataFromLatELon()
+  getApiDataFromLatELon(position.coords.latitude, position.coords.longitude);
 }
-function getApiDataFromLatELon(){
-    let promise = axios.get(`${url}data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`);
-    promise.then(displayData);
+
+function getApiDataFromLatELon(latitude, longitude) {
+  axios
+    .get(addApiKey(`${url}/data/2.5/weather?lat=${latitude}&lon=${longitude}`))
+    .then((response) => {
+      const data = response.data;
+
+      displayWeather(
+        data.name,
+        data.main.temp,
+        data.main.temp_min,
+        data.main.temp_max,
+        data.weather[0]
+      );
+    });
 }
-function getCityLocation(){
-    cityName = document.querySelector("input").value;
-    getApiDataFromCityName()
+
+function addApiKey(url) {
+  return `${url}&appid=${apiKey}`;
 }
-function getApiDataFromCityName(){
-    let promise = axios.get(`${url}geo/1.0/direct?q=${cityName}&limit=10&appid=${apiKey}`);
-    promise.then(getCityLatELon);
-    promise.catch((errorNum) => {
-        alert(`Select a valid location!`)
+
+function getApiDataFromCityName(cityName) {
+  axios
+    .get(addApiKey(`${url}/geo/1.0/direct?q=${cityName}&limit=1`))
+    .then((data) => {
+      const coords = data.data[0];
+      getApiDataFromLatELon(coords.lat, coords.lon);
     })
+    .catch(() => {
+      alert("Select a valid location!");
+    });
 }
-function getCityLatELon(apiResponse){
-    lat = apiResponse.data[0].lat
-    lon = apiResponse.data[0].lon
-    getApiDataFromLatELon()
+
+function kelvinToCelsius(K) {
+  return (K - 273.15).toFixed(1);
 }
-function displayData(apiResponse){
-    data = apiResponse.data;
-    convertTemperature()
-    console.log(data)
-}
-function convertTemperature(){
-    maxTemp = (data.main.temp_max - 273.15).toFixed(1);
-    minTemp = (data.main.temp_min - 273.15).toFixed(1);
-    temp = (data.main.temp - 273.15).toFixed(1);
-    displayWeather()
-}
-function displayWeather(){
-    weatherBox.innerHTML = `
-    <h2>${data.name}</h2>
-    <h1 class="current-weather">${temp}°C</h1>
-    <div class="min-max">
-        <h5>Min: ${minTemp}°C</h5>
-        <h5>Max: ${maxTemp}°C</h5>
+
+function displayWeather(cityName, temp, minTemp, maxTemp, weather) {
+  const iconSrc = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+
+  weatherBoxRef.innerHTML = `
+    <div class='row'>
+        <div class='col'>
+            <h2>${cityName}</h2>
+            <h1 class="current-weather">${kelvinToCelsius(temp)}°C</h1>
+        </div>
+        <img class='weather-icon' src="${iconSrc}"/>
     </div>
-    `
-    document.querySelector("body").classList.remove("default");
-    document.querySelector("body").classList.add(data.weather[0].main);
+    <div class="min-max">
+        <h5>Min: ${kelvinToCelsius(minTemp)}°C</h5>
+        <h5>Max: ${kelvinToCelsius(maxTemp)}°C</h5>
+    </div>
+    `;
+
+  weatherBoxRef.style.opacity = 1;
+
+  bodyRef.className = weather.main;
 }
